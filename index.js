7de21983a99abea85974d2ed36e5abb0dfdbc987
Catrong <105256455+Catrong@users.noreply.github.com>
2023-02-01 20:44:24 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+import fs from 'node:fs'
+
+logger.info('------φ^_^φ------')
+logger.info(` phi插件载入成功~`)
+logger.info(`-----------------`);
+
+
+//插件作者QQ号：1436375503
+//曲绘资源来源于网络
+//插件由个人独立编写，由于我没学过js，这个插件是一点一点照着其他大佬的插件抄的，如果有什么地方写的不对欢迎提出意见或做出修改
+//如果有什么好的建议也欢迎提出
+
+
+const files = fs.readdirSync('./plugins/phi-plugin/apps').filter(file => file.endsWith('.js'))
+
+let ret = []
+
+files.forEach((file) => {
+  ret.push(import(`./apps/${file}`))
+})
+
+ret = await Promise.allSettled(ret)
+
+let apps = {}
+for (let i in files) {
+  let name = files[i].replace('.js', '')
+
+  if (ret[i].status != 'fulfilled') {
+    logger.error(`载入插件错误：${logger.red(name)}`)
+    logger.error(ret[i].reason)
+    continue
+  }
+  apps[name] = ret[i].value[Object.keys(ret[i].value)[0]]
+}
+export { apps }
