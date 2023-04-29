9bc98036e057047cc9094fc40b51e5a3d462265c
jsbhgsn <1436375503@qq.com>
2023-04-29 22:21:44 +0800

    更新图鉴布局

--- model/songatlas.js
+++ model/songatlas.js
@@ -4,50 +4,53 @@ import puppeteer from './puppeteer.js'
 let interval = false
 class atlas {
 
-    async atlas(e, info) {
-        // 渲染数据
-        let data = {
-            /**曲名 */
-            song: info.song,
-            /**曲绘 */
-            illustration: info.illustration_big,
-            /**章节 */
-            chapter: info.chapter,
-            /**bpm */
-            bpm: info.bpm,
-            /**曲师 */
-            composer: info.composer,
-            /**时长 */
-            length: info.length,
-            /**画师 */
-            illustrator: info.illustrator,
-            /**谱面 */
-            SP: info.chart.SP,
-            AT: info.chart.AT,
-            IN: info.chart.IN,
-            HD: info.chart.HD,
-            EZ: info.chart.EZ,
-            /**其他消息（备注） */
-            othermsg: info.othermsg
-        }
-        // 渲染图片
-        return await puppeteer.render('atlas/atlas', {
-          ...data
-        }, {
-          e,
-          scale: 2.0
-        })
+  async atlas(e, info) {
+    // 渲染数据
+    let data = {
+      /**曲名 */
+      song: info.song,
+      /**曲绘 */
+      illustration: info.illustration_big,
+      /**章节 */
+      chapter: info.chapter,
+      /**bpm */
+      bpm: info.bpm,
+      /**曲师 */
+      composer: info.composer,
+      /**时长 */
+      length: info.length.replace(':', "'") + "''",
+      /**画师 */
+      illustrator: info.illustrator,
+      /**谱面 */
+      or: info.chart.or,
+      SP: info.chart.SP,
+      AT: info.chart.AT,
+      IN: info.chart.IN,
+      HD: info.chart.HD,
+      EZ: info.chart.EZ,
+      /**其他消息（备注） */
+      othermsg: info.othermsg,
+      /**预处理曲名字号 */
+      fontsize: info.song.length > 19 ? 24 : Math.min(94.4 - info.song.length * 2.6, 58)
     }
+    // 渲染图片
+    return await puppeteer.render('atlas/atlas', {
+      ...data
+    }, {
+      e,
+      scale: 2.0
+    })
+  }
 
 
-    async b19(e, data) {
-      return await puppeteer.render('b19/b19', {
-        ...data
-      }, {
-        e,
-        scale: 2.0
-      })
-    }
+  async b19(e, data) {
+    return await puppeteer.render('b19/b19', {
+      ...data
+    }, {
+      e,
+      scale: 2.0
+    })
+  }
 }
 
 
