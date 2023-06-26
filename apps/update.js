ea4d63b48fe1c8df1b23cdca51067c2565d6ee1f
jsbhgsn <1436375503@qq.com>
2023-06-26 11:30:15 +0800

    修复文案

--- apps/update.js
+++ apps/update.js
@@ -136,7 +136,7 @@ export class update extends plugin {
 
         let end = "";
         end =
-            "更多详细信息，请前往gitee查看\nhttps://github.com/Catrong/phi-plugin";
+            "更多详细信息，请前往github查看\nhttps://github.com/Catrong/phi-plugin";
 
         log = await this.makeForwardMsg(`phi-plugin更新日志，共${line}条`, log, end);
 
