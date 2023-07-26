dc78339f6dd823a0b970fe8281a1d0a0f9860a94
jsbhgsn <1436375503@qq.com>
2023-07-26 09:35:02 +0800

    更新文案

--- model/puppeteer.js
+++ model/puppeteer.js
@@ -39,7 +39,6 @@ export default new class newPuppeteer {
         Data.createDir(`data/html/${Plugin_Name}/${app}/${tpl}`, 'root')
         let data = {
             ...params,
-            b19size: Config.getDefOrConfig('config','b19size') / 100,
             _plugin: Plugin_Name,
             saveId: params.saveId || params.save_id || tpl,
             tplFile: `./plugins/${Plugin_Name}/resources/${app}/${tpl}.html`,
