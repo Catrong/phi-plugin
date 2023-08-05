1cdf8117d6d1cad4dddf0602098ce3f043243916
112121212167987534524 <117198625+112121212167987534524@users.noreply.github.com>
2023-08-05 18:36:45 +0800

    更改部分提示
    /bind xxx→/bind <sessionToken>

--- apps/b19.js
+++ apps/b19.js
@@ -47,7 +47,7 @@ export class phib19 extends plugin {
     async b19(e) {
         var save = await get.getsave(e.user_id)
         if (!save.session) {
-            e.reply(`你还没有绑定sessionToken哦！发送 /${Config.getDefOrConfig('config', 'cmdhead')} bind xxxx 进行绑定哦！`, true)
+            e.reply(`你还没有绑定sessionToken哦！发送 /${Config.getDefOrConfig('config', 'cmdhead')} bind <sessionToken> 进行绑定哦！`, true)
             return true
         }
 
@@ -135,7 +135,7 @@ export class phib19 extends plugin {
 
         var save = await get.getsave(e.user_id)
         if (!save.session) {
-            e.reply(`你还没有绑定sessionToken哦！发送 /${Config.getDefOrConfig('config', 'cmdhead')} bind xxxx进行绑定哦！`, true)
+            e.reply(`你还没有绑定sessionToken哦！发送 /${Config.getDefOrConfig('config', 'cmdhead')} bind <sessionToken>进行绑定哦！`, true)
             return true
         }
 
@@ -265,7 +265,7 @@ export class phib19 extends plugin {
     async singlescore(e) {
         var save = await get.getsave(e.user_id)
         if (!save.session) {
-            e.reply(`你还没有绑定sessionToken哦！发送 /${Config.getDefOrConfig('config', 'cmdhead')} bind xxxx 进行绑定哦！`, true)
+            e.reply(`你还没有绑定sessionToken哦！发送 /${Config.getDefOrConfig('config', 'cmdhead')} bind <sessionToken> 进行绑定哦！`, true)
             return true
         }
         var song = e.msg.replace(/[#/](.*)(score|单曲成绩)(\s*)/g, '')
@@ -364,7 +364,7 @@ export class phib19 extends plugin {
 
         var save = await get.getsave(e.user_id)
         if (!save.session) {
-            e.reply([segment.at(e.user_id), `你还没有绑定sessionToken哦！发送 /${Config.getDefOrConfig('config', 'cmdhead')} bind xxxx 进行绑定哦！`])
+            e.reply([segment.at(e.user_id), `你还没有绑定sessionToken哦！发送 /${Config.getDefOrConfig('config', 'cmdhead')} bind <sessionToken> 进行绑定哦！`])
             return true
         }
 
