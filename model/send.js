f2a91154b1c4a4b132ba3c66c90f4951071495ae
jsbhgsn <1436375503@qq.com>
2023-09-16 22:44:48 +0800

    fix

--- model/send.js
+++ model/send.js
@@ -34,7 +34,7 @@ class send {
 
         const save = await get.getsave(e.user_id)
 
-        if (!save) {
+        if (!save.session) {
             this.send_with_At(e, `请先绑定sessionToken哦！\n/${Config.getDefOrConfig('config', 'cmdhead')} bind <sessionToken>`)
             return false
         }
