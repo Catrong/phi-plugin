3b94aee6679190dd9e2c26b9eebd1f3396a42da4
Steve喵~ <117198625+112121212167987534524@users.noreply.github.com>
2023-09-23 10:48:58 +0800

    Update b19.js

--- apps/b19.js
+++ apps/b19.js
@@ -19,7 +19,7 @@ export class phib19 extends plugin {
             priority: 1000,
             rule: [
                 {
-                    reg: `^[#/]?(${Config.getDefOrConfig('config', 'cmdhead')})(\\s*)(b19|rks|pgr)$`,
+                    reg: `^[#/]?(${Config.getDefOrConfig('config', 'cmdhead')})(\\s*)(b19|rks|pgr|PGR|B19|RKS)$`,
                     fnc: 'b19'
                 },
                 {
