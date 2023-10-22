000c79a1b41477c4b2e7f2ebbdc268a941488645
jsbhgsn <1436375503@qq.com>
2023-10-22 14:23:00 +0800

    fix

--- apps/Dan.js
+++ apps/Dan.js
@@ -6,7 +6,7 @@ import Vika from '../model/Vika.js'
 import { segment } from 'oicq'
 import common from '../../../lib/common/common.js'
 
-const read = 'https://www.bilibili.com/read/cv24871082'
+const read = '（建设中）'
 const sheet = 'https://f.kdocs.cn/g/fxsg4EM2/'
 const word = `想要了解有关 Phigros民间段位标准 的有关信息可以查看这个专栏！\n${read}\n提交审核请前往这个链接哦！\n${sheet}\n`
 
