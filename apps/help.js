d0bd4ae5f68d5ba42a537e9afacfa9858d1b53d1
jsbhgsn <1436375503@qq.com>
2024-06-30 00:18:22 +0800

    别名去重

--- apps/help.js
+++ apps/help.js
@@ -31,8 +31,6 @@ export class phihelp extends plugin {
         })
 
     }
-
-    /**暂行帮助 */
     async help(e) {
         let head = Config.getDefOrConfig('config', 'cmdhead')
         head = head.match(RegExp(head))[0]
@@ -44,35 +42,6 @@ export class phihelp extends plugin {
             background: get.getill(get.illlist[Math.floor((Math.random() * (get.illlist.length - 1)))]),
             theme: pluginData?.plugin_data?.theme || 'star'
         }), true)
-
-        /**
-        await e.reply(`⌈phi-plugin 帮助⌋ (所有/均可用#代替,空格均可省略)\n` +
-            `⌈/${head}bind <sessionToken>⌋ 绑定sessionToken\n` +
-            `⌈/${head}unbind⌋ 删除sessionToken和存档记录\n` +
-            `⌈/${head}update⌋ 更新数据\n` +
-            `⌈/${head}pgr⌋ 获取b19图\n` +
-            `⌈/${head}info⌋ 获取个人信息统计\n` +
-            `⌈/${head}lvsco(scolv) <定数> <难度（可选）>⌋ 获取范围成绩图\n` +
-            `⌈/${head}best1(+)⌋ 获取文字版b19（或更多），最高b99\n` +
-            `⌈/${head}score <曲名>⌋ 获取单曲成绩\n` +
-            `⌈/${head}suggest⌋ 获取推分建议\n` +
-            `⌈/${head}song <曲名>⌋ 获取曲目图鉴\n` +
-            `⌈/${head}rand <条件>⌋ 随机曲目\n` +
-            `⌈/${head}ill <曲名>⌋ 查看曲目曲绘\n` +
-            `⌈/${head}data⌋ 查询data数量\n` +
-            `⌈/${head}re8⌋ 重置第八章剧情，不会清除成绩\n` +
-            `⌈/${head}search <条件 值>⌋ 检索曲目，支持BPM 定数(dif) 物量(cmb)\n` +
-            `⌈/${head}letter⌋ 根据字母猜曲名，⌈#出...⌋ 开指定的字母，⌈#第n个...⌋ 进行回答，⌈#字母答案⌋ 获取答案\n` +
-            `⌈/${head}guess⌋ 开始猜曲绘，回答直接发送，⌈#答案⌋ 结束\n` +
-            `⌈/${head}tk help⌋ 获取有关sessionToken的帮助\n` +
-            `---------------------\n` +
-            `⌈sign/签到⌋ 每日签到获取Notes和任务\n` +
-            `⌈task/我的任务⌋ 查看自己的任务\n` +
-            `⌈${head}send/送 <目标> <数量>⌋ 给某人转账，支持QQ号或@，将扣除部分手续费（20%）\n` +
-            `⌈retask/刷新任务⌋ 刷新任务，需要花费20Notes`
-
-        )
-        */
         return true
     }
 
