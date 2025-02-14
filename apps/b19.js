a4808507fe57631d2ea78d4e695a40e040f23e5e
1436375503@qq.com <1436375503@qq.com>
2025-02-15 01:21:07 +0800

    优化提示

--- apps/b19.js
+++ apps/b19.js
@@ -203,7 +203,7 @@ export class phib19 extends plugin {
 
         let res = [await altas.b19(e, data)]
         if (Math.abs(save_b19.com_rks - save.saveInfo.summary.rankingScore) > 0.1) {
-            res.push(`请注意，当前版本可能更改了计算规则\n计算rks: ${save_b19.com_rks}\n标准rks: ${save.saveInfo.summary.rankingScore}`)
+            res.push(`请注意，当前版本可能更改了计算规则\n计算rks: ${save_b19.com_rks}\n存档rks: ${save.saveInfo.summary.rankingScore}`)
         }
         send.send_with_At(e, res)
     }
