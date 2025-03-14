e202090b46a6c64bb089282a7229481bf8559544
Steve喵~ <117198625+S-t-e-v-e-e@users.noreply.github.com>
2025-03-15 06:23:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-//插件作者QQ号：1436375503
-//曲绘资源来源于网络
-//由于我没学过js，这个插件是一点一点照着其他大佬的插件抄的，如果有什么地方写的不对欢迎提出意见或做出修改
-//如果有什么好的建议也欢迎提出
-
-
-import Ver from './components/Version.js'
 import fs from 'node:fs'
-import chalk from 'chalk'
 if (!global.segment) {
     try {
         global.segment = (await import("icqq")).segment
@@ -14,8 +6,13 @@ if (!global.segment) {
         global.segment = (await import("oicq")).segment
     }
 }
-logger.mark(chalk.yellow('------φ^_^φ------'))
-logger.mark(chalk.yellow(`phi插件 ${Ver.ver} 载入中~ `))
+
+
+//插件作者QQ号：1436375503
+//曲绘资源来源于网络
+//由于我没学过js，这个插件是一点一点照着其他大佬的插件抄的，如果有什么地方写的不对欢迎提出意见或做出修改
+//如果有什么好的建议也欢迎提出
+logger.mark('------φ^_^φ------')
 
 const files = fs.readdirSync('./plugins/phi-plugin/apps').filter(file => file.endsWith('.js'))
 let errvis = false
@@ -27,12 +24,18 @@ files.forEach((file) => {
 
 ret = await Promise.allSettled(ret)
 
+//检查依赖
+import { checkPackage } from './components/check.js'
+let passed = await checkPackage()
+if (!passed) {
+    throw '缺少必要的依赖项'
+}
+
 let apps = {}
 for (let i in files) {
     let name = files[i].replace('.js', '')
 
     if (ret[i].status != 'fulfilled') {
-        logger.error('------QAQ!------')
         logger.error(`[phi-plugin]载入插件错误：${logger.red(name)}`)
         logger.error(ret[i].reason)
         errvis = true
@@ -44,6 +47,6 @@ for (let i in files) {
 export { apps }
 
 if (!errvis) {
-    logger.mark('phi插件载入成功~')
-    logger.mark(('本项目云存档功能由 ') + (chalk.yellow('7aGiven/PhigrosLibrary')) + (' 改写而来，感谢文酱的帮助！'))
+    logger.mark(` phi插件载入成功~`)
+    logger.mark(` 本项目云存档功能由 7aGiven/PhigrosLibrary 改写而来，感谢文酱的帮助！`)
 }
