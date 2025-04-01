e19fa10f33cf72d35d8b442d457878970a33a9c4
Catrong <1436375503@qq.com>
2025-04-01 21:27:05 +0800

    强迫症犯了

--- apps/b19.js
+++ apps/b19.js
@@ -73,7 +73,7 @@ const chap = {
     节奏大师: "Chapter EX-节奏大师精选集",
     齐秦太帅: "Chapter EX-节奏大师精选集",
     JZDS: "Chapter EX-节奏大师精选集",
-    EGTS: "Chapter Ex-EGTS 精选集",
+    EGTS: "Chapter EX-EGTS 精选集",
 }
 
 export class phib19 extends plugin {
