c71f3da3414712807c475476ee72e21e4706f42c
飞舞の废酱 <1436375503@qq.com>
2025-05-01 10:17:23 +0800

    评论增加对没有游玩过的判断

--- apps/phisong.js
+++ apps/phisong.js
@@ -762,10 +762,11 @@ export class phisong extends plugin {
             time: new Date(),
             comment: comment
         };
-        if (!songInfo.sp_vis) {
+        let songRecord = save.getSongsRecord(songId);
+        if (!songInfo.sp_vis && songRecord?.[rankNum]) {
             let { phi, b19_list } = await save.getB19(27)
             let spInfo = '';
-            let songRecord = save.getSongsRecord(songId)
+            
             for (let i = 0; i < phi.length; ++i) {
                 if (phi[i].id == songId && phi[i].rank == rankKind) {
                     spInfo = `Perfect ${i + 1}`;
