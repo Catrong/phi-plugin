6e0dc649e77621f94dc06f436269b86e7316e50c
飞舞の废酱 <1436375503@qq.com>
2025-05-01 08:38:06 +0800

    更正正则

--- apps/phisong.js
+++ apps/phisong.js
@@ -75,7 +75,7 @@ export class phisong extends plugin {
                     fnc: 'table'
                 },
                 {
-                    reg: `^[#/](${Config.getUserCfg('config', 'cmdhead')})(\\s*)(comment|cmt|评论|评价).*$`,
+                    reg: `^[#/](${Config.getUserCfg('config', 'cmdhead')})(\\s*)(comment|cmt|评论|评价)[\s\S]*$`,
                     fnc: 'comment'
                 },
                 {
