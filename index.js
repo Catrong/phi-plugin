5a3a28ac6ab3ff67a0f1caae6a0943075346442b
Steve喵~ <117198625+S-t-e-v-e-e@users.noreply.github.com>
2025-05-21 07:46:58 +0800

    ✓Update index.js (#223)
    * Update index.js
    
    小改一下，测试过了（
    
    * Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+import chalk from 'chalk';
 import fs from 'node:fs'
 import getInfo from './model/getInfo.js'
+import Version from './components/Version.js'
 if (!global.segment) {
     try {
         global.segment = (await import("icqq")).segment
@@ -13,7 +15,8 @@ if (!global.segment) {
 //曲绘资源来源于网络
 //由于我没学过js，这个插件是一点一点照着其他大佬的插件抄的，如果有什么地方写的不对欢迎提出意见或做出修改
 //如果有什么好的建议也欢迎提出
-logger.mark('------φ^_^φ------')
+logger.mark(chalk.rgb(255, 255, 0)('-------φ^_^φ-------'))
+logger.mark('正在载入phi插件...')
 
 const files = fs.readdirSync('./plugins/phi-plugin/apps').filter(file => file.endsWith('.js'))
 let errvis = false
@@ -39,6 +42,12 @@ for (let i in files) {
 export { apps }
 
 if (!errvis) {
-    logger.mark(` phi插件载入成功~`)
-    logger.mark(` 本项目云存档功能由 7aGiven/PhigrosLibrary 改写而来，感谢文酱的帮助！`)
+    logger.mark(chalk.rgb(178, 233, 250)('--------------------------------------'))
+    logger.mark(chalk.rgb(0, 183, 240)(`|phi插件${Version.ver}载入完成~`))
+    logger.mark(`|作者：@Cartong`)
+    logger.mark(chalk.rgb(0, 183, 240)(`|仓库地址：`))
+    logger.mark(`|https://github.com/Catrong/phi-plugin`)
+    logger.mark((chalk.rgb(0, 183, 240)`|本项目云存档功能由 7aGiven/PhigrosLibrary 改写而来`))
+    logger.mark(`|感谢文酱的帮助！`)
+    logger.mark(chalk.rgb(178, 233, 250)('--------------------------------------'))
 }
