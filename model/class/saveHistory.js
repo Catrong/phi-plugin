e8f8f812179a7284598cdbf650567617a33e0ce8
molanp <104612722+molanp@users.noreply.github.com>
2025-06-13 10:58:59 +0800

    refactor(model):移除冗余代码
    - 将 getRksAndDataLine 方法的逻辑改为调用 getRksLine 和 getDataLine 两个独立的方法

--- model/class/saveHistory.js
+++ model/class/saveHistory.js
@@ -242,105 +242,18 @@ export default class saveHistory {
 
     /**
      * 折线图数据
-     * @returns 
+     * @returns {Object} 
+     * @property {Array<Array<number>>} rks_history - RKS历史折线数据点集合
+     * @property {number[]} rks_range - RKS值域范围 [min, max]
+     * @property {number[]} rks_date - RKS时间戳范围 [最早, 最晚]
+     * @property {number[][]} data_history - 数据历史折线数据点集合
+     * @property {(number | string)[]} data_range - 数据值域范围，可能为数值数组 [min, max] 或格式化字符串数组，如 ['1MiB', '4GiB']
+     * @property {number[]} data_date - 数据时间戳范围 [最早, 最晚]
      */
     getRksAndDataLine() {
-
-        let rks_history_ = []
-        let user_rks_data = this.rks
-        let rks_range = [MAX_DIFFICULTY, 0]
-        let rks_date = [];
-        let rks_history = []
-
-        if (user_rks_data.length) {
-            rks_date = [new Date(user_rks_data[0].date).getTime(), 0]
-            for (let i in user_rks_data) {
-                user_rks_data[i].date = new Date(user_rks_data[i].date)
-                if (i <= 1 || user_rks_data[i].value != rks_history_[rks_history_.length - 2].value) {
-                    rks_history_.push(user_rks_data[i])
-                    rks_range[0] = Math.min(rks_range[0], user_rks_data[i].value)
-                    rks_range[1] = Math.max(rks_range[1], user_rks_data[i].value)
-                } else {
-                    rks_history_[rks_history_.length - 1].date = user_rks_data[i].date
-                }
-                rks_date[1] = user_rks_data[i].date.getTime()
-            }
-
-            for (let i in rks_history_) {
-
-                i = Number(i)
-
-                if (!rks_history_[i + 1]) break
-                let x1 = fCompute.range(rks_history_[i].date, rks_date)
-                let y1 = fCompute.range(rks_history_[i].value, rks_range)
-                let x2 = fCompute.range(rks_history_[i + 1].date, rks_date)
-                let y2 = fCompute.range(rks_history_[i + 1].value, rks_range)
-                rks_history.push([x1, y1, x2, y2])
-            }
-            if (!rks_history.length) {
-                rks_history.push([0, 50, 100, 50])
-            }
-        }
-
-
-        let data_history_ = []
-        let user_data_data = this.data
-        let data_range = [1e9, 0]
-        let data_date = []
-        let data_history = []
-
-        if (user_data_data.length) {
-            data_date = [new Date(user_data_data[0].date).getTime(), 0]
-            for (let i in user_data_data) {
-                let value = user_data_data[i]['value']
-                user_data_data[i].value = (((value[4] * 1024 + value[3]) * 1024 + value[2]) * 1024 + value[1]) * 1024 + value[0]
-                user_data_data[i].date = new Date(user_data_data[i].date)
-                if (i <= 1 || user_data_data[i].value != data_history_[data_history_.length - 2].value) {
-                    data_history_.push(user_data_data[i])
-                    data_range[0] = Math.min(data_range[0], user_data_data[i].value)
-                    data_range[1] = Math.max(data_range[1], user_data_data[i].value)
-                } else {
-                    data_history_[data_history_.length - 1].date = user_data_data[i].date
-                }
-                data_date[1] = user_data_data[i].date.getTime()
-            }
-
-            for (let i in data_history_) {
-
-                i = Number(i)
-
-                if (!data_history_[i + 1]) break
-                let x1 = fCompute.range(data_history_[i].date, data_date)
-                let y1 = fCompute.range(data_history_[i].value, data_range)
-                let x2 = fCompute.range(data_history_[i + 1].date, data_date)
-                let y2 = fCompute.range(data_history_[i + 1].value, data_range)
-                data_history.push([x1, y1, x2, y2])
-            }
-
-
-            let unit = ["KiB", "MiB", "GiB", "TiB", "Pib"]
-
-            for (let i in [1, 2, 3, 4]) {
-                if (Math.floor(data_range[0] / (Math.pow(1024, i))) < 1024) {
-                    data_range[0] = `${Math.floor(data_range[0] / (Math.pow(1024, i)))}${unit[i]}`
-                }
-            }
-
-            for (let i in [1, 2, 3, 4]) {
-                if (Math.floor(data_range[1] / (Math.pow(1024, i))) < 1024) {
-                    data_range[1] = `${Math.floor(data_range[1] / (Math.pow(1024, i)))}${unit[i]}`
-                }
-            }
-        }
-
-        return {
-            rks_history,
-            rks_range,
-            rks_date,
-            data_history,
-            data_range,
-            data_date
-        }
+        rks = this.getRksLine()
+        data = this.getDataLine()
+        return { ...rks, ...data }
     }
 
     getRksLine() {
