903539d0ded2e1881d6c64e053c9cba4bb8bf088
Catrong <1436375503@qq.com>
2026-03-06 19:53:03 +0800

    fix: watch README.md for version updates

--- components/Version.js
+++ components/Version.js
@@ -3,6 +3,7 @@ import fs from 'fs'
 import cfg from '../../../lib/config/config.js'
 import { pluginRoot } from '../model/path.js'
 import logger from './Logger.js'
+import chokidar from 'chokidar'
 const README_path = `${pluginRoot}/README.md`
 const yunzai_ver = `v${cfg.package.version}`
 
@@ -28,4 +29,19 @@ let Version = {
     phigrosVerNum: phigrosVerNum,
     yunzai: yunzai_ver,
 };
+
+chokidar.watch(README_path).on('change', () => {
+    try {
+        const logs = fs.readFileSync(README_path, 'utf8')
+        currentVersion = 'v' + (/插件版本\-([0-9\.]+)/.exec(logs)?.[1] ?? '')
+        phigrosVer = /Phigros\-([0-9\.]+)/.exec(logs)?.[1] ?? ''
+        phigrosVerNum = Number(/PhigrosVer\-([0-9]+)/.exec(logs)?.[1] ?? 0)
+        Version.ver = currentVersion
+        Version.phigros = phigrosVer
+        Version.phigrosVerNum = phigrosVerNum
+    } catch (e) {
+        logger.error(e)
+    }
+})
+
 export default Version
