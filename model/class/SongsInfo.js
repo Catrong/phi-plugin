ebd26c6f41572449b029d40ab0a95d4a64603cfb
Temmie0125 <1179755948@qq.com>
2026-03-10 17:13:22 +0800

    添加Phigros独占曲信息以及图鉴显示

--- model/class/SongsInfo.js
+++ model/class/SongsInfo.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: Temmie0125 1179755948@qq.com
+ * @Date: 2025-11-29 00:47:13
+ * @LastEditors: Temmie0125 1179755948@qq.com
+ * @LastEditTime: 2026-03-10 16:49:38
+ * @FilePath: \实验与作业e:\bot\Yunzai\plugins\phi-plugin\model\class\SongsInfo.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import getInfo from "../getInfo.js"
 import Chart from "./Chart.js"
 
@@ -32,6 +40,8 @@ export default class SongsInfo {
         this.illustrator = data.illustrator
         /** @type {string} 特殊信息 */
         this.spinfo = data.spinfo
+        /** @type {boolean} 独占曲标记 */
+        this.isOriginal = data.isOriginal
         /**
          * 谱面详情
          * @type {Partial<Record<allLevelKind, Chart>>} 
